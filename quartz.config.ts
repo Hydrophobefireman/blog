fcc622aabab1a62f0c77512c9e78978d37198561
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-12-20 14:47:27 -0500

    init

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,11 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "HPFM's Blog",
     enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    baseUrl: "quartz.jzhao.xyz",
+    enablePopovers: false,
+    analytics: null,
+    baseUrl: "blog.hpfm.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
